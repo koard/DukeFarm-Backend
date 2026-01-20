8c9ad5ad3fc8802f8e4b8a663f53294b53a2d0e8
Ratchanon Kulpatrakorn <koardtep@gmail.com>
2026-01-20 19:27:22 +0700

    Fix: API 404 error by updating src/routes/dashboard.routes.ts Changed the route path

--- src/routes/dashboard.routes.ts
+++ src/routes/dashboard.routes.ts
@@ -4,6 +4,6 @@ import { DashboardController } from '../controllers/dashboard.controller';
 
 const router = Router();
 
-router.get('/groups/:groupType', authMiddleware, DashboardController.getDashboardByFarmType);
+router.get('/:groupType', authMiddleware, DashboardController.getDashboardByFarmType);
 
 export { router as dashboardRouter };
